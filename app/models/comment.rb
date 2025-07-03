1dad4e889a58266b0a1b5edfbc2ac362ecc46a8c
Said-MZ <saidzain897@gmail.com>
2025-07-03 17:03:51 +0300

    Add status field to articles and comments; update forms and views for status management

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,17 @@
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
+
+  validates :commenter, presence: true
+  validates :body, presence: true
+
+  # Set default status to public
+  after_initialize :set_default_status, if: :new_record?
+
+  private
+
+  def set_default_status
+    self.status ||= "public"
+  end
 end
